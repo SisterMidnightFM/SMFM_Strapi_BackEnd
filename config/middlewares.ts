d5156a16853cba60114c0d36352b6d587a045ea7
Tom Willis <tomwillis@Toms-MacBook-Pro.local>
2025-10-26 17:26:32 +0000

    updating server

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -1,12 +1,20 @@
-export default [
-  'strapi::logger',
+module.exports = [
   'strapi::errors',
   'strapi::security',
   'strapi::cors',
   'strapi::poweredBy',
+  'strapi::logger',
   'strapi::query',
   'strapi::body',
-  'strapi::session',
+  'strapi::session', // Add this
   'strapi::favicon',
   'strapi::public',
+  {
+    name: 'strapi::session',
+    config: {
+      cookie: {
+        secure: false, // 👈 allow cookies over HTTP
+      },
+    },
+  },
 ];
