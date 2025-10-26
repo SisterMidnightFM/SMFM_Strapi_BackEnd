cc22a914bf9c60e2b655a48adb4a0e50877327e1
Tom Willis <tomwillis@Toms-MacBook-Pro.local>
2025-10-26 18:30:00 +0000

    updating code

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -1,4 +1,4 @@
-export default [
+export default ({ env }) => [
   'strapi::logger',
   'strapi::errors',
   'strapi::security',
@@ -6,7 +6,17 @@ export default [
   'strapi::poweredBy',
   'strapi::query',
   'strapi::body',
-  'strapi::session',
+  {
+    name: 'strapi::session',
+    config: {
+      rolling: true,
+      renew: true,
+      cookie: {
+        secure: env.bool('IS_PROXIED', false),
+        sameSite: env.bool('IS_PROXIED', false) ? 'none' : 'lax',
+      },
+    },
+  },
   'strapi::favicon',
   'strapi::public',
 ];
