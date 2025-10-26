89f2fb9876de9f7e9ec0b6b40a863ddbf8397e9b
Tom Willis <tomwillis@Toms-MacBook-Pro.local>
2025-10-26 18:11:47 +0000

    updating code

--- config/server.ts
+++ config/server.ts
@@ -1,7 +1,9 @@
-module.exports = ({ env }) => ({
+export default ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
-  url: env('PUBLIC_URL', 'https://smfm-strapi-backend.onrender.com'),
-  proxy: true, // 👈 Tells Strapi it’s behind a reverse proxy (Render)
-  app: { keys: env.array('APP_KEYS') },
+  url: env('URL', 'http://localhost:1337'),
+  proxy: env.bool('IS_PROXIED', false),
+  app: {
+    keys: env.array('APP_KEYS'),
+  },
 });
